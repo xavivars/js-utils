2713b83e9ed88ed23d512cc9341dcef18b9b0171
Xavi Ivars <xavi.ivars@gmail.com>
2007-11-09 12:13:48 +0000

    new class: percent-slider
    git-svn-id: svn+ssh://infobenissa.com/home/xavivars/svn/jsutils/trunk@9 1a4362c0-5b38-0410-95b1-aaf384f500a0

---
+++ slider.js
@@ -0,0 +1,29 @@
+﻿// Archivo JScript
+
+function Slider() 
+{
+    this.rows = new Array();
+}
+
+Slider.prototype.addRow=function(id,name,display,value,type)
+{
+    var nr = {
+        'id':       id,
+        'name':     name,
+        'display':  display,
+        'value':    value,
+        'type':     type    
+    }
+    
+    rows.add(nr);
+}
+
+// fila del slider
+
+
+// id,nom,visualitzar,valor,tipus
+
+//new Array ('1000','Element 0',false,'15.7',SliderMathTypePercent); 
+
+
+
