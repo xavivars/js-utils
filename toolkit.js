bd975ac7ea8768e279ec1bb22404033b2468c38f
Xavi Ivars <xavi.ivars@gmail.com>
2007-11-15 11:30:02 +0000

    ADDED: TOOLKIT now has an Hex-Dec number converter object
    git-svn-id: svn+ssh://infobenissa.com/home/xavivars/svn/jsutils/trunk@31 1a4362c0-5b38-0410-95b1-aaf384f500a0

--- toolkit.js
+++ toolkit.js
@@ -322,3 +322,29 @@ if(typeof getEvent == 'undefined')
 		return window.event ? window.event : aEvent;
 	}
 }
+
+if (typeof Hex == 'undefined')
+{
+	Hex = function (){
+		var values = new Array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F");
+
+		function toHex(nm)
+		{
+		    var hexaDec = Math.floor(nm/16);
+
+		    var hexaUni = nm - (hexaDec * 16);
+
+			return values[hexaDec] + values[hexaUni];
+		}
+
+		function toDecimal(nm)
+		{
+			return parseInt(''+nm,16);
+		}
+
+		return {
+			toHex : toHex,
+			toDecimal: toDecimal
+		}
+	}();
+}
